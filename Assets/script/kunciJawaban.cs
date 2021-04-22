c33859f61ef25d2b69beb64eaf7dfdfa4640c1bb
dzikrihf07 <dzikrihf@gmail.com>
2021-04-22 16:03:07 +0700

    Penambahan fitur skor dan tambah soal

---
+++ Assets/script/kunciJawaban.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class kunciJawaban : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt ("skor", 0);
+    }
+
+    public void jawaban(bool jawab) {
+        if (jawab) {
+            int skor = PlayerPrefs.GetInt ("skor") + 10;
+            PlayerPrefs.SetInt ("skor", skor);
+        } else {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
